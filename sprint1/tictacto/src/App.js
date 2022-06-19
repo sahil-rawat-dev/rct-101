e6ba89acdd652b31f06f7e752b42a7bfa2ab2c57
sahil-rawat-dev <sahilrawat2710@gmail.com>
2022-06-19 20:47:19 +0530

    done

---
+++ sprint1/tictacto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTacToe from './components/tictacto';
+
+function App() {
+	return (
+		<div className='App'>
+			<TicTacToe />
+		</div>
+	);
+}
+
+export default App;
